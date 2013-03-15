f4a7a295bd8a3a13238b7f76cb11f745db392fef
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2013-03-15 11:12:12 +0000

    bump version to 0.4.4

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
     [clj-http "0.6.4"]
     ])
 
-(defproject clj-duedil "0.4.3"
+(defproject clj-duedil "0.4.4"
   :description "clj-duedil : a clojure library for the duedil api"
 
   :url "http://github.com/trampoline/clj-duedil"
